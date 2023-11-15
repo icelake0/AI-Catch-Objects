a22217d30c4339e43cf5b5bac59878b1b87235d7
Gbemileke AJIBOYE <gbemileke@seamlesshr.com>
2023-11-15 12:19:05 +0000

    implement agent

---
+++ agent.py
@@ -0,0 +1,104 @@
+import torch
+import random
+import numpy as np
+from collections import deque
+from environmant import FragilePackagesStore
+from model import Linear_QNet, QTrainer
+from support import plot
+
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LR = 0.001
+
+class Agent:
+    
+    def __init__(self):
+        self.n_games = 1
+        self.epsilon = 0 # randomness
+        self.gamma = 0.9 # discount rate
+        self.memory = deque(maxlen=MAX_MEMORY) # pops left when memory is full
+        self.model = Linear_QNet(4, 256, 3)
+        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
+        
+
+    
+    def get_state(self, store):
+        return np.array(store.get_state(), dtype=int)
+    
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done)) # pops left when memory is full
+    
+    def train_long_memory(self):
+        if len(self.memory) > BATCH_SIZE:
+            mini_sample = random.sample(self.memory, BATCH_SIZE) # list of tuples
+        else:
+            mini_sample = self.memory
+
+        states, actions, rewards, next_states, dones = zip(*mini_sample)
+        self.trainer.train_step(states, actions, rewards, next_states, dones)
+    
+    def train_short_memory(self, state, action, reward, next_state, done):
+        self.trainer.train_step(state, action, reward, next_state, done)
+    
+    def get_action(self, state):
+        # random moves: tradeoff exploration / exploitation
+        self.epsilon = 80 - self.n_games #The more games we play the smaller our epsilon gets hence the less randome moves we make untill we stop making random move
+        final_move = [0,0,0]
+        if random.randint(0, 200) < self.epsilon:
+            move = random.randint(0, 2)
+            final_move[move] = 1
+        else:
+            state0 = torch.tensor(state, dtype=torch.float)
+            prediction = self.model(state0)
+            move = torch.argmax(prediction).item()
+            final_move[move] = 1
+
+        return final_move
+    
+    
+def train():
+    plot_scores = []
+    plot_mean_scores = []
+    total_score = 0
+    record = 0
+    agent = Agent()
+    store = FragilePackagesStore()
+    while True:
+        # get old state
+        state_old = agent.get_state(store)
+        
+        # get move
+        final_move = agent.get_action(state_old)
+        
+        # perform move and get new state
+        reward, done, score = store.monitor_store(final_move)
+        state_new = agent.get_state(store)
+        
+        # train short memory
+        agent.train_short_memory(state_old, final_move, reward, state_new, done)
+        
+         # remember
+        agent.remember(state_old, final_move, reward, state_new, done)
+        
+        if done:
+            # train long memory, plot result
+            store.setup_store()
+            agent.n_games += 1
+            agent.train_long_memory()
+            
+        if score > record:
+                record = score
+                agent.model.save()
+        
+        print('Game', agent.n_games, 'Score', score, 'Record:', record)
+        #plot model progress
+        plot_scores.append(score)
+        total_score += score
+        mean_score = total_score / agent.n_games
+        plot_mean_scores.append(mean_score)
+        plot(plot_scores, plot_mean_scores)
+            
+
+
+if __name__ == '__main__':
+    train()
--- environmant.py
+++ environmant.py
@@ -1,30 +1,10 @@
 import pygame, sys, random
+import numpy as np
 
 
 class FragilePackagesStore:
     def __init__(self):
         self.setup_store()
-
-        # Packages that falls randomly
-        self.package = pygame.Rect(
-            self.screen_width / 2 - 15, self.screen_height / 2 - 15, 30, 30
-        )
-        
-        # Net for catching package
-        self.net = pygame.Rect(
-            self.screen_width / 2 - 70, self.screen_height - 20, 140, 10
-        )
-
-        # Environment Variables
-        self.package_speed_x = 7 * random.choice((1, -1))
-        self.package_speed_y = 7
-        self.net_speed = 0
-        self.package_moving = False
-        self.score_time = True
-
-        # Score Text
-        self.net_score = 0
-        self.basic_font = pygame.font.Font("freesansbold.ttf", 50)
     
     def setup_store(self):
         # General setup
@@ -44,6 +24,30 @@ class FragilePackagesStore:
         self.bg_color = pygame.Color("grey12")
         self.score_color = pygame.Color("gray")
         
+        # Packages that falls randomly
+        self.package = pygame.Rect(
+            self.screen_width / 2 - 15, self.screen_height / 2 - 15, 30, 30
+        )
+        
+        # Net for catching package
+        self.net_width = 140
+        self.net = pygame.Rect(
+            self.screen_width / 2 - 70, self.screen_height - 20, self.net_width , 10
+        )
+
+        # Environment Variables
+        self.package_speed_x = 7 * random.choice((1, -1))
+        self.package_speed_y = 7
+        self.net_speed = random.choice((6, -6))
+        self.package_moving = False
+        self.score_time = True
+        self.net_failed = False
+        self.reward = 0
+
+        # Score Text
+        self.net_score = 0
+        self.basic_font = pygame.font.Font("freesansbold.ttf", 50)
+        
         
     def setup_package(self):
         self.package.x += self.package_speed_x
@@ -57,17 +61,27 @@ class FragilePackagesStore:
 
         if self.package.bottom >= self.screen_height:
             self.score_time = pygame.time.get_ticks()
-            self.net_score -= 10
+            self.net_failed = True
+            self.reward = -10
             self.score_color = pygame.Color("red")
             self.package_speed_y *= -1
 
         if self.package.colliderect(self.net):
             self.score_time = pygame.time.get_ticks()
             self.net_score += 10
+            self.reward = 10
             self.score_color = pygame.Color("green")
             self.package_speed_y *= -1
 
-    def move_net(self):
+    def move_net(self, action):
+        
+        if np.array_equal(action, [1, 0, 0]):
+            self.net_speed = -6
+        elif np.array_equal(action, [0, 1, 0]):
+            self.net_speed = 0
+        else: # [0, 0, 1]
+            self.net_speed = 6
+            
         self.net.x += self.net_speed
 
         if self.net.left <= 0:
@@ -87,25 +101,14 @@ class FragilePackagesStore:
             self.score_color = pygame.Color("gray")
             self.score_time = None
 
-    def monitor_store(self):
-        while True:
+    def monitor_store(self, action):
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_LEFT:
-                        self.net_speed -= 6
-                    if event.key == pygame.K_RIGHT:
-                        self.net_speed += 6
-                if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_LEFT:
-                        self.net_speed += 6
-                    if event.key == pygame.K_RIGHT:
-                        self.net_speed -= 6
-
+                    
             self.setup_package()
-            self.move_net()
+            self.move_net(action)
 
             # Visuals
             #self.screen.fill(self.bg_color)
@@ -125,7 +128,13 @@ class FragilePackagesStore:
             )
 
             pygame.display.flip()
-            self.clock.tick(60)
-
-
-store = FragilePackagesStore().monitor_store()
+            self.clock.tick(999999)
+            return self.reward, self.net_failed, self.net_score
+    
+    def get_state(self):
+        return [
+            self.net.x > self.package.x + 30,
+            self.net.x < self.package.x and self.net.x + self.net_width  > self.package.x + 30,
+            self.net.x + self.net_width < self.package.x,
+            self.net_speed > 0
+        ]
